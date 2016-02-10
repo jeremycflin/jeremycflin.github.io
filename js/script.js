c252453195b3a6d8e35531144b3bb83f2dd751a6
Jeremy C.F. Lin <jeremycflin@gmail.com>
2016-02-10 13:17:56 -0800

    test

---
+++ js/script.js
@@ -0,0 +1,42 @@
+d3.tsv("assets/portfolio.tsv",function(portfolio) {
+  window.portfolio = portfolio;
+
+  var container = d3.select("#clip");
+
+  var thumbnail = container.selectAll(".story")
+      .data(portfolio)
+    .enter().append("div")
+      .classed("story",true)
+
+  var link = thumbnail.append("a")
+      .classed("url",true)
+      .attr("target","_blank")
+      .attr("href",function(d) { return d.url; });
+
+  link.append("img")
+      .attr("src",function(d) { return "assets/img/" + d.imglink + ".png"; })
+      .classed("imglink", true);
+
+  link.append("a")
+      .classed("work-label",true)
+      .text(function(d) { return d.name; });
+
+  thumbnail.append("p")
+      .classed("pub",true)
+      .text(function(d){return d.pub})
+
+  thumbnail.append("p")
+      .classed("note",true)
+      .text(function(d){return d.partner})
+});
+
+
+jQuery(document).ready(function($){
+      $(function(){
+            $(".element").typed({
+                  strings: ["code", "report", "write", "draw", "design", "build"],
+                  typeSpeed: 10,
+                  loop: true
+            });
+      });
+});
