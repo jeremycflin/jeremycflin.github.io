071b5160c499cb6bc68835a1eb50f56024f43950
Jeremy C.F. Lin <jeremycflin@gmail.com>
2016-07-28 22:03:15 -0400

    clean up

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 d3.tsv("assets/portfolio.tsv",function(portfolio) {
-  window.portfolio = portfolio;
 
   var container = d3.select("#clip");
 
   var thumbnail = container.selectAll(".story")
       .data(portfolio)
-    .enter().append("div")
+      .enter()
+      .append("div")
       .classed("story",true)
 
   var link = thumbnail.append("a")
@@ -21,7 +21,7 @@ d3.tsv("assets/portfolio.tsv",function(portfolio) {
 
   link.append("a")
       .classed("work-label",true)
-      .text(function(d) { return d.name; });
+      .text(function(d) { return  d.name; });
 
   link.append("p")
       .classed("pub",true)
@@ -29,47 +29,8 @@ d3.tsv("assets/portfolio.tsv",function(portfolio) {
 
   link.append("p")
       .classed("note",true)
-      .text(function(d){return d.partner})
+      .text(function(d){return "With " + d.partner})
 });
-
-// function animationTrigger (){
-//   var n = 1200;
-
-//   var whiteblue = d3.interpolateRgb("#eee", "#B21F26"),
-//       blueorange = d3.interpolateRgb("#B21F26", "orange"),
-//       orangewhite = d3.interpolateRgb("orange", "#eee");
-
-//   var container = d3.select("#test").append("svg")
-
-
-
-//   container
-//     .data(d3.range(n))
-//     .enter().append("rect")
-//     .attr("class","animation")
-//     .attr("z-index", -1000)
-//     .transition()
-//       .delay(function(d, i) { return i + Math.random() * n / 4; })
-//       .ease(d3.easeLinear)
-//       .on("start", function repeat() {
-//           d3.active(this)
-//               .styleTween("background-color", function() { return whiteblue; })
-//             .transition()
-//               .delay(1000)
-//               .styleTween("background-color", function() { return blueorange; })
-//             .transition()
-//               .delay(1000)
-//               .styleTween("background-color", function() { return orangewhite; })
-//             .transition()
-//               .delay(n)
-//               .on("start", repeat);
-//         });
-//   }
-
-
-
-
-
 
 jQuery(document).ready(function($){
       $(function(){
